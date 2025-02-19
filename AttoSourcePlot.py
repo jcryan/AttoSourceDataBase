2b58a51efd56c59a68ae04675d487c392b38ce11
jcryan <jcryan@slac.stanford.edu>
2025-02-18 18:19:31 -0800

    Update AttoSourcePlot.py
    update plotting functions for clarity.

--- AttoSourcePlot.py
+++ AttoSourcePlot.py
@@ -1,4 +1,4 @@
-from AttoSourceFigClass import AttoSourcePlot
+from AttoSourceFig import AttoSourcePlot
 from adjustText import adjust_text
 
 atto = AttoSourcePlot(source_type='HHG', pulse_type='IAP', x_axis='PhotonEng', y_axis='PulseEng')
@@ -20,4 +20,9 @@ atto.fig.legend(bbox_to_anchor=(1.5, 1), loc='outside upper right')
 atto.ax.grid()
 atto.ax.set(xlabel='Photon Energy (eV)', ylabel='Pulse Energy (J)')
 
-atto.emblish()
+lines = atto.ax.get_lines()
+used = set()
+ind = [i for i,line in enumerate(lines) if (line.get_marker(), line.get_color()) not in used and (used.add((line.get_marker(), line.get_color())) or True)]
+atto.ax.legend([lines[i] for i in ind],['HHG IAP', 'HHG APT', 'FEL IAP', 'FEL APT'], loc='lower right')
+
+atto.decorate()
